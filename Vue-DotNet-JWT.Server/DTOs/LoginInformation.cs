7e721630ccef96eb152179f623f2ec399326bdde
manhattaa <fadynhatta@gmail.com>
2024-10-30 10:14:23 +0100

    Created a LoginInformation DTO

---
+++ Vue-DotNet-JWT.Server/DTOs/LoginInformation.cs
@@ -0,0 +1,8 @@
+﻿namespace Vue_DotNet_JWT.Server.DTOs
+{
+    public class LoginInformation
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
